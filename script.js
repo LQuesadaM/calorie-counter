48cfa2460fb0c161fe7aad8543d0df4302fb10f2
LQuesadaM <luisquesada163@gmail.com>
2024-02-12 08:19:21 -0300

    clean the string value and ensure you have a number

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const entryDropdown = document.querySelector("#entry-dropdown");
 const addEntryButton = document.querySelector("#add-entry");
 const clearButton = document.querySelector("#clear");
 const output = document.querySelector("#output");
+
+let isError = false;
+
+function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex, "");
+}
