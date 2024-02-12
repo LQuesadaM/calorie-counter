af92dd7f885be01452b72bf2d6ce3fe529143466
LQuesadaM <luisquesada163@gmail.com>
2024-02-12 19:16:05 -0300

    fix bug whem add entryNumbers

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function isInvalidInput (str) {
 
 function addEntry () {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
     const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
     <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
     <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
