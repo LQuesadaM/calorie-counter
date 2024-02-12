b190f7a0e7c63474e01261bef3a030686c4a3fab
LQuesadaM <luisquesada163@gmail.com>
2024-02-12 19:06:38 -0300

    add entries to the calorie counter

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ function isInvalidInput (str) {
     const regex = /\d+e\d+/gi;
     return str.match(regex);
 }
+
+function addEntry () {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">
+    `;
+
+    targetInputContainer.innerHTML += HTMLString;
+}
