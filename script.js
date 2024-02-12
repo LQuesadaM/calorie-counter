c27a291e92225e5e1d10d2368bd23a471270d8ef
LQuesadaM <luisquesada163@gmail.com>
2024-02-12 07:52:21 -0300

    variable declaration

---
+++ script.js
@@ -0,0 +1,6 @@
+const calorieCounter = document.querySelector("#calorie-counter");
+const budgetNumberInput = document.querySelector("#budget");
+const entryDropdown = document.querySelector("#entry-dropdown");
+const addEntryButton = document.querySelector("#add-entry");
+const clearButton = document.querySelector("#clear");
+const output = document.querySelector("#output");
