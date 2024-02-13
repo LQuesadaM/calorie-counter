289ed81fa1c10dc22b1dca61a642e20d2d84c5c1
LQuesadaM <luisquesada163@gmail.com>
2024-02-13 08:12:53 -0300

    get list of elements

--- script.js
+++ script.js
@@ -30,8 +30,14 @@ function addEntry () {
 }
 
 function calculateCalories (e) {
-    e.preventDefault()
+    e.preventDefault();
     isError = false;
+
+    const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type=number]");
+    const lunchNumberInputs = document.querySelectorAll("#lunch input[type=number]");
+    const dinnerNumberInputs = document.querySelectorAll("#dinner input[type=number]"); 
+    const snacksNumberInputs = document.querySelectorAll("#snacks input[type=number]");
+    const exerciseNumberInputs = document.querySelectorAll("#exercise input[type=number]");
 }
 
 function getCaloriesFromInputs (list) {
