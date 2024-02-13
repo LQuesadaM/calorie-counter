ccd34aad89b52667b4742f794e52d2f1c38d649d
LQuesadaM <luisquesada163@gmail.com>
2024-02-13 08:05:25 -0300

    preventDefault reload page

--- script.js
+++ script.js
@@ -29,6 +29,11 @@ function addEntry () {
     targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
 
+function calculateCalories (e) {
+    e.preventDefault()
+    isError = false;
+}
+
 function getCaloriesFromInputs (list) {
     let calories = 0;
 
