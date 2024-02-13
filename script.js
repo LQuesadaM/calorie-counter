e49fb1b08f03076c79af9ddc3271e87d0c882529
LQuesadaM <luisquesada163@gmail.com>
2024-02-13 08:24:12 -0300

    extract the calorie

--- script.js
+++ script.js
@@ -38,6 +38,13 @@ function calculateCalories (e) {
     const dinnerNumberInputs = document.querySelectorAll("#dinner input[type=number]"); 
     const snacksNumberInputs = document.querySelectorAll("#snacks input[type=number]");
     const exerciseNumberInputs = document.querySelectorAll("#exercise input[type=number]");
+
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+    const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+    const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+    const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 }
 
 function getCaloriesFromInputs (list) {
